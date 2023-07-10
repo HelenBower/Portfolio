582e6346565e50b131e12d98970309a8171e1a5d
HelenBower <136718674+HelenBower@users.noreply.github.com>
2023-07-10 12:27:50 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let pinktab= document.querySelector(".aboutButton")
+let pg1= document.querySelector("index.html")
+let pg2=document.querySelector("two.html")
+
+pinktab.onclick=function(){
+   fi.location.href="two.html"
+}
